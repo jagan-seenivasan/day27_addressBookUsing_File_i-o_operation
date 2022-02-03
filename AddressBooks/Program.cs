1421f12087b64b02e81f53fa10ba3c35844e01d1
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2022-02-03 21:06:09 +0530

    adding Contact

---
+++ AddressBooks/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AddressBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Welcome To Address Book System!");
+            AddrBook.GetCustomer();
+            AddrBook.ListingPeople();
+
+        }
+    }
+}
